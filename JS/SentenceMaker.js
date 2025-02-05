38680b8bc840012bdcf24e3bd5974fa096ce071e
Cyrilange <cyril.r.a.salamite@gmail.com>
2025-02-05 20:44:41 +0100

    jsday

---
+++ JS/SentenceMaker.js
@@ -0,0 +1,21 @@
+let adjective = "beautiful";
+let noun = "cat";
+let verb = "jumps";
+let place = "on the table";
+let adjective2 = "fluffy";
+let noun2 = "tail";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("First story: " + firstStory);
+
+adjective = "gigantic"
+noun = "robot"
+verb = "dances"
+place = "under the neon lights"
+adjective2 = "shiny"
+noun2 = "armor"
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("\nSecond story: " + secondStory)
---
+++ JS/trviatBot.js
@@ -0,0 +1,16 @@
+console.log("Hello! I'm your coding fun fact guide!")
+let botName = "Speaky";
+let botLocation = "Malga";
+let favoriteLanguage = "C";
+const welcomeBot = "My name is " + botName + " and I live on " + botLocation + ".";
+console.log(welcomeBot);
+const botLen = "My favorite programming language is " + favoriteLanguage + ".";
+console.log(botLen);
+
+let codingFact = "Fun fact about " + favoriteLanguage + ".";
+console.log(codingFact);
+codingFact = "New fun fact about " + favoriteLanguage + ".";
+console.log(codingFact)
+codingFact = "An other New fun fact about " + favoriteLanguage + ".";
+console.log(codingFact)
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
